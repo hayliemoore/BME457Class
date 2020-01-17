62c2f068fc2248f916cf60c399194975452d7fab
haylie moore <haylie.moore@duke.edu>
2020-01-17 14:30:01 -0500

    features include add, sub, multiple, divide

--- calculator.py
+++ calculator.py
@@ -2,14 +2,35 @@
 
 def add (x, y):
   z = x + y
-  print(" {} + {} = {}  ".format (x, y, z))
+  print(" {} + {} = {} ".format(x, y, z))
   return z
+def subtract (x, y):
+    z = x - y
+    print(" {} - {} = {} ".format(x, y, z))
+    return z
+def multiply (x, y):
+    z = x * y
+    print(" {} * {} = {} ".format(x, y, z))
+    return z
+def divide (x, y):
+    z = x / y
+    print(" {} / {} = {} ".format(x, y, z))
+    return z
   
-x = input ( "enter a letter: ")
+x = input ("enter a letter: ")
 print("yout entered {}".format(x))
 if x == "a":
-  z = add(56, 73)
-  
+  z = add(47, 7)
+  print (z)
+if x == "b":
+    z = subtract(47, 7)
+    print (z)
+if x == "c":
+    z = multiply(47, 7)
+    print (z)
+if x == "d":
+    z = divide(47, 7)
+    print (z)
 elif x == "s": 
   a = 20
   b = -3
