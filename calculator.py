a3f553a98d8cd0636a7a211b4d6b563d0455e121
hayliemoore <59743195+hayliemoore@users.noreply.github.com>
2020-01-17 12:38:14 -0500

    created a new file

---
+++ calculator.py
@@ -0,0 +1,22 @@
+# calculator.py
+
+def add (x, y):
+  z = x + y
+  print(" {} + {} = {}  ".format (x, y, z))
+  return z
+  
+x = input ( "enter a letter: ")
+print("yout entered {}".format(x))
+if x == "a".
+  d = add(56, 73)
+  if z> 100:
+    print("this number is too high")
+elif x == "s". 
+  a = 20
+  b = -3
+  c = a - b
+  print(" {} + {} = {}  ".format (a, b, c))
+else:
+  print("The {} comman is not recognized."format(x))
+  
+  print("Done")
