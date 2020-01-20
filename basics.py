d567158e0ec049f7d8ffd0e83e2039fa642af606
haylie moore <haylie.moore@duke.edu>
2020-01-19 19:47:19 -0500

    updated basics

--- basics.py
+++ basics.py
@@ -1,3 +1,22 @@
-# basics.py
-x = input ("enter a letter:")
-print("you entered {}.".format(x))
+# basics
+x = input("enter a letter: ")
+print("you entered {}".format(x))
+if x == "a":
+    a = 1
+    b = 2
+    c = a + b
+    print ("{} + {} = {}".format(a, b, c))
+elif x == "s":
+      a = 20
+      b = -3
+      c = a - b
+      print("{} - {} = {}".format(a, b, c))
+elif x == "m":
+    a = 2
+    b = 4
+    c = a * b
+    print("{} * {} = {}".format(a, b, c))
+else:
+    print("the {} command is not recognized.".format(x))
+        
+print("Done")
