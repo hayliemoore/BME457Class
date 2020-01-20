61b11e9459290dccc08fe730e65dee858c53f6a7
haylie moore <haylie.moore@duke.edu>
2020-01-20 13:53:25 -0500

    changed the variables so that int would work

--- calculator.py
+++ calculator.py
@@ -1,9 +1,6 @@
 # calculator.py
 
-a = input ("enter first number: ")
-b = input ("enter second number: ")
-x = int(a) 
-y = int(b)
+
 
 
 def add (x, y):
@@ -22,18 +19,26 @@ def divide (x, y):
     z = x / y
     print(" {} / {} = {} ".format(x, y, z))
     return z
+    
+a = input ("enter first number: ")
+b = input ("enter second number: ")
+x = int(a) 
+y = int(b)
   
-x = input ("enter a letter: ")
+sym = input ("enter a letter: ")
 print("you entered {}".format(x))
-if x == "a":
-  z = add(int(a), int(b))
+if sym == "a":
+  z = add(x, y)
   print (z)  
-if x == "b":
+elif sym == "b":
     z = subtract(int(a), int(b))
     print (z)
-if x == "c":
+elif sym == "c":
     z = multiply(int(a), int(b))
     print (z)
-if x == "d":
+elif sym == "d":
     z = divide(int(a), int(b))
     print (z)
+else:
+    print("the {} command is not recognized.".format(x))   
+print("Done")
